4743906f9aafa0806569c8d138a72458f389e124
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-05 22:23:29 +0900

    이동

---
+++ src/StackQueue.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackQueue {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>();
+
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+
+        queue.offer("1");
+        queue.offer("2");
+        queue.offer("3");
+        //스택은 접시와 같고
+        System.out.println("=Stack=");
+        while (!stack.empty()) {
+            System.out.println("s = " + stack.pop());
+        }
+        //큐는 줄과 같다.
+        System.out.println("=queue");
+        while (!queue.isEmpty()) {
+            System.out.println("q = " + queue.poll());
+        }
+    }
+}
