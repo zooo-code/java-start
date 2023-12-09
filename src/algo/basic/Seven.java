53e64129246f14b01c7abd604df7d90e7c2a3519
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-09 17:25:19 +0900

    study

---
+++ src/algo/basic/Seven.java
@@ -0,0 +1,14 @@
+package algo.basic;
+
+import java.util.Scanner;
+
+public class Seven {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
