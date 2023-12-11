0c5b7809ed37396fee6af040cf06b38b47fc03bf
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-11 21:16:44 +0900

    study

---
+++ src/algo/Level1/FruitMarket.java
@@ -0,0 +1,23 @@
+package algo.Level1;
+
+import java.util.*;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/135808
+public class FruitMarket {
+
+
+    public static void main(String[] args) {
+        int k = 3;
+        int m = 4;
+        int[] score = {1,2,3,1,2,3,1};
+
+        FruitMarket fruitMarket = new FruitMarket();
+        int solution = fruitMarket.solution(k, m, score);
+        System.out.println(solution);
+    }
+
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        return answer;
+    }
+}
