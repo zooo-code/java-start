c38e63286ef08097bf9e0c0fa7eab1b2e3931556
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-12 22:02:26 +0900

    study

---
+++ src/algo/Level1/NumberPartner.java
@@ -0,0 +1,19 @@
+package algo.Level1;
+
+public class NumberPartner {
+
+
+    public static void main(String[] args) {
+
+        String X = "100";
+        String Y = "2345";
+        String solution = new NumberPartner().solution(X, Y);
+        System.out.println("solution = " + solution);
+    }
+
+
+    public String solution(String X, String Y) {
+        String answer = "";
+        return answer;
+    }
+}
