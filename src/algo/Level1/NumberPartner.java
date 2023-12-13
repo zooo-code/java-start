bd28b7f926ae669b23bf373800389aedeff7a79a
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-13 21:34:18 +0900

    study

--- src/algo/Level1/NumberPartner.java
+++ src/algo/Level1/NumberPartner.java
@@ -1,5 +1,8 @@
 package algo.Level1;
+import java.util.*;
+import java.util.stream.Collectors;
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/131128
 public class NumberPartner {
 
 
@@ -13,7 +16,35 @@ public class NumberPartner {
 
 
     public String solution(String X, String Y) {
-        String answer = "";
-        return answer;
+        Map<String, Integer> xMap = new HashMap<>();
+        Map<String, Integer> yMap = new HashMap<>();
+
+        List<String> list = new ArrayList<>();
+
+        for(String key: X.split("")) {
+            xMap.put(key, xMap.getOrDefault(key, 0)+1);
+        }
+
+        for(String key: Y.split("")) {
+            yMap.put(key, yMap.getOrDefault(key, 0)+1);
+        }
+
+
+        for(String key: xMap.keySet()) {
+            if(!yMap.containsKey(key)) continue;
+
+            int length = Math.min(xMap.get(key), yMap.get(key));
+            for(int i = 0; i < length; i++) {
+                list.add(key);
+            }
+        }
+
+        String result = list.stream()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.joining());
+
+        if(result.isEmpty()) return "-1";
+        if(result.replaceAll("0", "").isEmpty()) return "0";
+        return result;
     }
 }
---
+++ src/algo/Level1/kakaoPrivacyInformation.java
@@ -0,0 +1,13 @@
+package algo.Level1;
+//https://school.programmers.co.kr/learn/courses/30/lessons/150370?language=java
+public class kakaoPrivacyInformation {
+
+
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int[] answer = {};
+        return answer;
+    }
+}
