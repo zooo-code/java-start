6d19db2702aeacba954283cab88ac053e1354eac
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-15 23:27:34 +0900

    study

---
+++ src/algo/Level1/sinGO.java
@@ -0,0 +1,17 @@
+package algo.Level1;
+//https://school.programmers.co.kr/learn/courses/30/lessons/92334
+public class sinGO {
+    public static void main(String[] args) {
+
+        sinGO sinGO = new sinGO();
+        String[] idList = {"muzi", "frodo", "apeach", "neo"};
+        String[] report={"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+        sinGO.solution(idList,report,k);
+    }
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
