a856183f03b890077a16c34408a28e9801b4ebdc
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-17 00:35:45 +0900

    study

---
+++ src/algo/Level2/OpenChat.java
@@ -0,0 +1,16 @@
+package algo.Level2;
+
+public class OpenChat {
+
+    public static void main(String[] args) {
+
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        String[] solution = new OpenChat().solution(record);
+        System.out.println("solution = " + solution);
+    }
+
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
