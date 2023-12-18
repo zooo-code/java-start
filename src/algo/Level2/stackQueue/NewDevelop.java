44eeaa9284d295756df155051f178cc02d884fd6
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-18 20:34:43 +0900

    study

---
+++ src/algo/Level2/stackQueue/NewDevelop.java
@@ -0,0 +1,20 @@
+package algo.Level2.stackQueue;
+
+import java.util.*;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java
+public class NewDevelop {
+
+    public static void main(String[] args) {
+        NewDevelop newDevelop = new NewDevelop();
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        int[] solution = newDevelop.solution(progresses, speeds);
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
