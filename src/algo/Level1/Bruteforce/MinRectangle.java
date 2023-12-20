420c704e2106c722db9e2d2dca559c54418746dc
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-20 18:37:46 +0900

    study

---
+++ src/algo/Level1/Bruteforce/MinRectangle.java
@@ -0,0 +1,20 @@
+package algo.Level1.Bruteforce;
+import java.util.*;
+
+
+public class MinRectangle {
+
+
+    public static void main(String[] args) {
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        int solution = new MinRectangle().solution(sizes);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        return answer;
+    }
+
+
+}
