1d15ff7a14f8e88368be3232a77817ccfd502b42
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-21 10:23:35 +0900

    study

---
+++ src/algo/Level2/DFSBFS/TargetNum.java
@@ -0,0 +1,17 @@
+package algo.Level2.DFSBFS;
+
+public class TargetNum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+        new TargetNum().solution(numbers,target);
+    }
+
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+}
