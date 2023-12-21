a3bc479c2dfdc9db830925ab2af8cc51b669af0c
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-21 10:21:17 +0900

    study

--- src/algo/Level1/Bruteforce/MinRectangle.java
+++ src/algo/Level1/Bruteforce/MinRectangle.java
@@ -1,5 +1,4 @@
 package algo.Level1.Bruteforce;
-import java.util.*;
 
 
 public class MinRectangle {
@@ -13,6 +12,25 @@ public class MinRectangle {
 
     public int solution(int[][] sizes) {
         int answer = 0;
+        int maxRow = 0;
+        int maxCol = 0;
+        // 모든 배열을 돌면서 가장 긴 가로와 세로를 찾는다. 일단 가로세로 구분 x
+        for(int i = 0 ; i < sizes.length ; i++){
+            int[] rec = sizes[i];
+            int row = rec[0];
+            int col = rec[1];
+            if (rec[0] < rec[1]){
+                row = rec[1];
+                col = rec[0];
+            }
+            if (maxRow < row){
+                maxRow =row;
+            }
+            if (maxCol < col){
+                maxCol = col;
+            }
+        }
+        answer = maxCol* maxRow;
         return answer;
     }
 
