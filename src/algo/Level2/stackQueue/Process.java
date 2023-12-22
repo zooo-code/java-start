ebcf09c58e479449dfd503e9dedc2155f1bcef28
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-22 20:29:33 +0900

    study

---
+++ src/algo/Level2/stackQueue/Process.java
@@ -0,0 +1,13 @@
+package algo.Level2.stackQueue;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java
+public class Process {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+}
