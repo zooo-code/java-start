07062358e7521c04492d28d2f0b17af3d1ec4934
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-23 20:06:32 +0900

    study

--- src/algo/Level2/stackQueue/Process.java
+++ src/algo/Level2/stackQueue/Process.java
@@ -3,7 +3,11 @@ package algo.Level2.stackQueue;
 public class Process {
 
     public static void main(String[] args) {
-
+        Process process = new Process();
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+        int solution = process.solution(priorities, location);
+        System.out.println("solution = " + solution);
     }
 
     public int solution(int[] priorities, int location) {
