9d340459085bbb84c2cda3e4e74e1cf5ee4a6e78
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-24 20:28:25 +0900

    study

--- src/algo/Level1/sort/KthNumber.java
+++ src/algo/Level1/sort/KthNumber.java
@@ -1,9 +1,14 @@
 package algo.Level1.sort;
 //https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java
+import java.util.*;
+
 public class KthNumber {
 
     public static void main(String[] args) {
-
+        int[] array = {};
+        int[][] commands ={{}};
+        int[] solution = new KthNumber().solution(array, commands);
+        System.out.println(Arrays.toString(solution));
     }
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = {};
