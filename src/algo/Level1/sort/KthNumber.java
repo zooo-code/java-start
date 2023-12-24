37a23a4dd5800ceda585fac0d4bbd08cefcedb31
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-24 20:26:58 +0900

    study

---
+++ src/algo/Level1/sort/KthNumber.java
@@ -0,0 +1,12 @@
+package algo.Level1.sort;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java
+public class KthNumber {
+
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/algo/Level2/sort/BiggestNumber.java
@@ -0,0 +1,9 @@
+package algo.Level2.sort;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42746
+public class BiggestNumber {
+
+    public String solution(int[] numbers) {
+        String answer = "";
+        return answer;
+    }
+}
