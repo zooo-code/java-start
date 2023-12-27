7229a7590ff46220adb0788caa1fe9a20bbf123b
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-27 15:52:20 +0900

    study

---
+++ src/algo/Level2/greedy/JoyStick.java
@@ -0,0 +1,16 @@
+package algo.Level2.greedy;
+
+import java.util.*;
+
+public class JoyStick {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
