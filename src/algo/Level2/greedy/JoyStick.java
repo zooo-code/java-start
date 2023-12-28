0ca0492a3b9c3e6301c89bc88e2f7458cfe2d1ce
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-12-28 18:17:24 +0900

    study

--- src/algo/Level2/greedy/JoyStick.java
+++ src/algo/Level2/greedy/JoyStick.java
@@ -1,16 +1,41 @@
 package algo.Level2.greedy;
-
+//https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java
+//https://velog.io/@jeeseob5761/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%A1%B0%EC%9D%B4%EC%8A%A4%ED%8B%B1
+//todo 나중에 보기
 import java.util.*;
 
 public class JoyStick {
 
     public static void main(String[] args) {
-
-
+        JoyStick joyStick = new JoyStick();
+        String name = "JEROEN";
+        int solution = joyStick.solution(name);
+        System.out.println("solution = " + solution);
     }
 
     public int solution(String name) {
         int answer = 0;
+        int length = name.length();
+
+        int index; // 다음 값들을 확인할 때 사용
+        int move = length - 1; // 좌우 움직임 수를 체크
+
+        for(int i = 0; i < name.length(); i++){
+            //일단 상하로 움직이는 가장 최소의 값은 무조건 이동이 필요한 경우 이므로 더한다.
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            index = i + 1;
+            // 연속되는 A 갯수 확인
+            while(index < length && name.charAt(index) == 'A'){
+                index++;
+            }
+
+            // 순서대로 가는 것과, 뒤로 돌아가는 것 중 이동수가 적은 것을 선택
+            move = Math.min(move, i * 2 + length - index);
+            // 2022년 이전 테스트 케이스만 확인하면 여기까지해도 정답처리가 되기 때문에, 이전 정답들에는 여기까지만 정리되어 있지만,
+            // BBBBAAAAAAAB 와 같이, 처음부터 뒷부분을 먼저 입력하는 것이 더 빠른 경우까지 고려하려면 아래의 코드가 필요합니다.
+            move = Math.min(move, (length - index) * 2 + i);
+        }
         return answer;
     }
 }
