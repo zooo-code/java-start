243cc93386f7faa1ab38c62808540fd6e942dfe6
zoo-code <75787789+zooo-code@users.noreply.github.com>
2024-01-11 10:08:57 +0900

    study

---
+++ src/algo/Level2/EmoticonDiscountOffer.java
@@ -0,0 +1,19 @@
+package algo.Level2;
+
+import java.util.*;
+//https://school.programmers.co.kr/learn/courses/30/lessons/150368?language=java
+
+public class EmoticonDiscountOffer {
+
+    public static void main(String[] args) {
+        int[][] users = {{40, 10000},{25, 10000}};
+        int[] emoticons ={7000,9000};
+        int[] solution = new EmoticonDiscountOffer().solution(users, emoticons);
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+    public int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
