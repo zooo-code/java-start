5a96909a1ed75362529666490e95f582f813d506
zoo-code <75787789+zooo-code@users.noreply.github.com>
2024-01-16 21:31:49 +0900

    study

---
+++ src/algo/Level1/HidePhoneNumber.java
@@ -0,0 +1,18 @@
+package algo.Level1;
+//https://school.programmers.co.kr/learn/courses/30/lessons/12948
+public class HidePhoneNumber {
+
+
+    public static void main(String[] args) {
+
+        String phone_number = "01033334444";
+
+        String solution = new HidePhoneNumber().solution(phone_number);
+        System.out.println("solution = " + solution);
+    }
+
+    public String solution(String phone_number) {
+        String answer = "";
+        return answer;
+    }
+}
