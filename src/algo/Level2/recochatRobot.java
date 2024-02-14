6f87a1f1c9f79d037fd7cdf61fc7e6d97168adaf
zoo-code <75787789+zooo-code@users.noreply.github.com>
2024-02-14 17:50:48 +0900

    study algo

---
+++ src/algo/Level2/recochatRobot.java
@@ -0,0 +1,18 @@
+package algo.Level2;
+//https://school.programmers.co.kr/learn/courses/30/lessons/169199?language=java
+public class recochatRobot {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String[] board) {
+        int answer = 0;
+        return answer;
+    }
+
+
+
+
+}
