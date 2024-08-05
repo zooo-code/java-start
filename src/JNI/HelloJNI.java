6c7b2d7192e81d72bc8f7caf7521129cfa209a67
zoo-code <kyj20908@naver.com>
2024-08-05 23:20:39 +0900

    JNI 실습

--- src/JNI/HelloJNI.java
+++ src/JNI/HelloJNI.java
@@ -1,18 +1,9 @@
 package JNI;
 
 public class HelloJNI {
+    native void printHello();
 
-    // 네이티브 메소드 선언
-    public native String sayHello();
-
-    // 네이티브 라이브러리 로드
     static {
-        System.loadLibrary("hello");
-    }
-
-    public static void main(String[] args) {
-        HelloJNI helloJNI = new HelloJNI();
-        String message = helloJNI.sayHello(); // 네이티브 메소드 호출
-        System.out.println(message); // 네이티브 메소드 결과 출력
+        System.loadLibrary("hellojni");
     }
 }
